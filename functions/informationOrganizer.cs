4cbae76581f2f8a8ce8e44197c0fc23c03a5358f
OliverSolberg <oliver.solberg@instech.no>
2025-04-24 14:24:57 +0200

    Added several functions in order to read a json file and create a string from the hobbies array of Person

---
+++ functions/informationOrganizer.cs
@@ -0,0 +1,16 @@
+public class informationOrganizer
+{
+    public string listOutHobbies(Person person)
+    {
+        if(person.Hobbies == null || person.Hobbies.Length > 0)
+            return "I have no hobbies";
+
+        int length = person.Hobbies.Length;
+
+        if(length < 2)
+            return "My hobby is " + person.Hobbies[0]
+        
+        string result = string.Join(" ", person.Hobbies.Take(length -1));
+        return "My hobbies are " + result + " and " + person.Hobbies[^1];
+    }
+}
---
+++ model/person.cs
@@ -0,0 +1,8 @@
+public static class Person
+{
+    int Id {get; set;}
+    string? Name {get; set;}
+    int Age {get; set;}
+    string[] Hobbies {get; set;}
+    string? City {get; set;}
+}
