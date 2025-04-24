f3cafb145683b48d96022203c9fbc39d0e60352a
OliverSolberg <oliver.solberg@instech.no>
2025-04-24 11:40:20 +0200

    Committing to future

--- aspnetcoreapp/obj/Debug/net9.0/aspnetcoreapp.AssemblyInfo.cs
+++ aspnetcoreapp/obj/Debug/net9.0/aspnetcoreapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aspnetcoreapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64a688faa50d7f1ec146a2ef9b7b8eb79d5229ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("aspnetcoreapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aspnetcoreapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
